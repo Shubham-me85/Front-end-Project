e177ab9b92aa905f80b152c3e4d639dd76bd0755
Shubham <shubham.my85@gmail.com>
2022-07-23 19:06:20 +0530

    Today's progress

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const sections = document.querySelectorAll('.section')
 const sectBtns = document.querySelectorAll('.controls')
 const sectBtn = document.querySelectorAll('.control')
-const allsections = document.querySelectorAll('.main-content')
+const allSections = document.querySelectorAll('.main-content')[0]
 
 function PageTransitions(){
-    for(let i = 0; i < sectBtn.length; i++){
+    
+    for(let i=0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn';
         })
-    }    
+    }
+
+    
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+    })
+
 }
 
 PageTransitions();

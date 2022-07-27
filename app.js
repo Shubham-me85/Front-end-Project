a464185e1fc36d77b6c9d2122cb38514e11e45ff
Shubham <shubham.my85@gmail.com>
2022-07-27 19:07:28 +0530

    Today's commit

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ function PageTransitions(){
     
     allSections.addEventListener('click', (e) =>{
         const id = e.target.dataset.id;
+        if(id){
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active')
+
+
+        }
+
     })
 
 }
